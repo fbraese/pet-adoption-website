e0ee6bf58dd508ccc12648fd789bda698c857e1d
xnannamanx <william.a.lozano@gmail.com>
2021-04-20 08:39:48 -0700

    scheme and seeder commit

--- server/db/Seeder.js
+++ server/db/Seeder.js
@@ -4,11 +4,37 @@ import LineReader from "line-reader"
 import { fileURLToPath } from "url"
 
 const pool = new pg.Pool({
-  connectionString: "postgres://postgres:password@localhost:5432/<yourDBnameHere>" })
+  connectionString: "postgres://postgres:password@localhost:5432/pet-adoption-website-development"
+})
+
+//setup __dirname to work with ESM
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const petTypePath = path.join(__dirname, "../../pet_types.txt")
 
 class Seeder {
   static async seed() {
     // your seeder code here
+    LineReader.eachLine(petTypePath, async (line, last, done) => {
+      const [type, img_url, description] = line.split(";")
+      // build our SQL query string
+      const queryString = "INSERT INTO pet_types ( type, img_url, description) VALUES ($1, $2, $3);"
+
+      //execute our query
+      try {
+        const result = await pool.query(queryString, [ type, img_url, description])
+        if (last) {
+          //drain the pool because we're done connecting
+          console.log("Seeding Complete")
+          pool.end()
+        }
+        done()
+      } catch (error) {
+        console.log(`Error: ${error}`)
+        done()
+      }
+    })
   }
 }
 
