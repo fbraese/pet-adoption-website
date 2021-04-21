548ab22a7f7f31e24900be8e2e4c90636033a725
Michael <67665934+mjstafford@users.noreply.github.com>
2021-04-21 09:51:44 -0700

    current work (WIP)

---
+++ client/components/PetAdoptionForm.js
@@ -0,0 +1,49 @@
+import React, {useState} from "react"
+
+const PetAdoptionForm = (props) => {
+  const [adoption, setAdoption] = useState({
+    name = "",
+    phoneNumber = "",
+    email = "",
+    homeStatus = "",
+    applicationStatus = "",
+    adoptablePetId = "",
+  })
+
+  const handleChange = (event) => {
+    setAdoption({
+      ...adoption,
+      [event.currentTaget.id]:event.currentTaget.value 
+    })
+  }
+  //left of at needing to put the handleChange (plus all the rest of the steps)
+  return (
+    <form>
+      <label htmlFor="name">
+        <input type="text" id="name" name="name" />
+      </label>
+
+      <label htmlFor="phoneNumber">
+        <input type="text" id="phoneNumber" name="phoneNumber" />
+      </label>
+
+      <label htmlFor="email">
+        <input type="text" id="email" name="email" />
+      </label>
+
+      <label htmlFor="homeStatus">
+        <input type="text" id="homeStatus" name="homeStatus" />
+      </label>
+
+      <label htmlFor="applicationStatus">
+        <input type="text" id="applicationStatus" name="applicationStatus" />
+      </label>
+      
+      <label htmlFor="adoptablePetId">
+        <input type="text" id="adoptablePetId" name="adoptablePetId" />
+      </label>
+    </form>
+  )
+}
+
+export default PetAdoptionForm
--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS pet_types CASCADE;
 DROP TABLE IF EXISTS adoptable_pets;
+DROP TABLE IF EXISTS adoption_applications;
 
 CREATE TABLE pet_types(
     id SERIAL PRIMARY KEY,
@@ -17,4 +18,14 @@ CREATE TABLE adoptable_pets(
     adoption_story TEXT NOT NULL,
     available_for_adoption BOOLEAN DEFAULT 't',
     pet_type_id INTEGER REFERENCES pet_types(id)
+);
+
+CREATE TABLE adoption_applications(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    phone_number INTEGER NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    home_status VARCHAR(255) NOT NULL,
+    application_status VARCHAR(255) DEFAULT 'pending',
+    adoptable_pet_id INTEGER REFERENCES adoptable_pets(id)
 );
