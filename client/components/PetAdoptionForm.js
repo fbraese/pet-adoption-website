ddc69d63cea33beb2908e591c1cfe6b50846fb88
Michael <67665934+mjstafford@users.noreply.github.com>
2021-04-21 14:15:05 -0700

    WIP

--- client/components/PetAdoptionForm.js
+++ client/components/PetAdoptionForm.js
@@ -1,50 +1,131 @@
+//aded import to petType page for testing! remove when done!!!
 import React, {useState} from "react"
 
+import ErrorList from "./ErrorsList"
+
 const PetAdoptionForm = (props) => {
+  const [errors, setErrors] = useState({})
   const [adoption, setAdoption] = useState({
-    name = "",
-    phoneNumber = "",
-    email = "",
-    homeStatus = "",
-    applicationStatus = "",
-    adoptablePetId = "",
+    name: "",
+    phoneNumber: "",
+    email: "",
+    homeStatus: "",
+    applicationStatus: "",
+    adoptablePetId: "",
   })
 
-  const handleChange = (event) => {
+  const handleInputChange = (event) => {
     setAdoption({
       ...adoption,
-      [event.currentTaget.id]:event.currentTaget.value
+      [event.currentTarget.id]: event.currentTarget.value
     })
   }
-  //left of at needing to put the handleChange (plus all the rest of the steps)
+
+  const submitHandler = (event) => {
+    event.preventDefault()
+    if (isValid()) {
+      addPetForAdoption()
+    } else {
+      console.log("form Not submitted!")
+    }
+  }
+
+  const addPetForAdoption = async () => {
+    try {
+      //do we need a new api endpoint ?? how do we interact with DB with all pets
+      const response = await fetch("/api/v1/petType", {
+        method: "POST",
+        headers: new Headers ({
+          "Content-Type": "application/json",
+        }),
+        body: JSON.stringify(adoption)
+      })
+      if(!response.ok) {
+        const errorMessage = `${response.status} ${response.statusText}`
+        throw new Error(errorMessage)
+      }
+      const responseBody = await response.json()
+      console.log("posted succesfully", responseBody)
+      setErrors({}) // to force re-render on success
+      clearForm()
+    } catch (error) {
+      console.error("error in form fetch POST", error)
+    }
+  }
+
+  const isValid = () => {
+    let submitErrors = {}
+    const requiredFields = ['name', 'phoneNumber', 'email', 'homeStatus']
+    requiredFields.forEach(field => {
+      if (adoption[field].trim() === "") {
+        submitErrors = {
+          ...submitErrors,
+          [field]: "is blank"
+        }
+      } 
+    })
+    setErrors(submitErrors)
+    return Object.entries(submitErrors).length === 0 && submitErrors.constructor === Object
+  }
+
+  const clearForm = () => {
+    setAdoption({
+      name: "",
+      phoneNumber: "",
+      email: "",
+      homeStatus: "",
+      applicationStatus: "",
+      adoptablePetId: "",
+    })
+  }
+
   return (
-  <form onFormChange={ handleChange }>
     <div>
-      <label htmlFor="name">
-        <input type="text" id="name" name="name" />
-      </label>
+      <ErrorList errors={errors}/>
+      <form onSubmit={submitHandler}>
+        <div>
+          <label htmlFor="name">Name:
+            <input 
+              type="text" 
+              id="name" 
+              name="name" 
+              onChange={handleInputChange}
+              value={adoption.name}
+            />
+          </label>
 
-      <label htmlFor="phoneNumber">
-        <input type="text" id="phoneNumber" name="phoneNumber" />
-      </label>
+          <label htmlFor="phoneNumber">Phone Number:
+            <input 
+              type="text" 
+              id="phoneNumber" 
+              name="phoneNumber" 
+              onChange={handleInputChange}
+              value={adoption.phoneNumber}
+            />
+          </label>
 
-      <label htmlFor="email">
-        <input type="text" id="email" name="email" />
-      </label>
+          <label htmlFor="email">Email:
+            <input 
+              type="text" 
+              id="email" 
+              name="email" 
+              onChange={handleInputChange}
+              value={adoption.email}
+            />
+          </label>
 
-      <label htmlFor="homeStatus">
-        <input type="text" id="homeStatus" name="homeStatus" />
-      </label>
+          <label htmlFor="homeStatus">Home status:
+            <select name="homeStatus" id="homeStatus" onChange={handleInputChange} value={adoption.homeStatus}>
+            <option value=""></option>
+              <option value="own">Own</option>
+              <option value="rent">Rent </option>
+            </select>
+          </label>
 
-      <label htmlFor="applicationStatus">
-        <input type="text" id="applicationStatus" name="applicationStatus" />
-      </label>
-      
-      <label htmlFor="adoptablePetId">
-        <input type="text" id="adoptablePetId" name="adoptablePetId" />
-      </label>
+          <input type="submit" value="Submit Form"/>
+        </div>
+      </form>
     </div>
-  </form>
   )
 }
 
